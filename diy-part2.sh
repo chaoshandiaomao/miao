4b2de9a91ec9a0b784b388b66d2317f8bfb6a0f0
chaoshandiaomao <128349784+chaoshandiaomao@users.noreply.github.com>
2023-06-02 18:58:18 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,4 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.88.1/g' package/base-files/files/bin/config_generate
 sed -i "/CYXluq4wUazHjmCDBCqXF/d" package/lean/default-settings/files/zzz-default-settings
-
+sed -i 's/\/bin\/login/\/bin\/login -f root/' /etc/config/ttyd
